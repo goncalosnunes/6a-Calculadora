76cd2f748578d18d8f5514e74793ee29ff134f6b
goncalo <goncalosilvan@gmail.com>
2019-03-22 20:20:13 +0000

    Adição do código do controller para processar os dados na calculadora

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -13,5 +13,30 @@ namespace Calculadora.Controllers
         {
             return View();
         }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(String visor, string btn)
+        {
+
+            switch (btn)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    break;
+            }
+            ViewBag.Resposta = visor;
+            return View();
+        }
     }
+
+
 }
